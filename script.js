88d29babb7a9fe8dd7d2ec131dc4ad36f648fef2
Shreyas754 <72546939+Shreyas754@users.noreply.github.com>
2021-05-09 14:19:53 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,27 @@
+canvas -
+document.getElementById("myCanvas");
+ctx = canvas.getContext("2d");
+
+canvas.addEventListener("mousedown",my_mousedown);
+
+function my_mousedown(e) {
+    color = document.getElementById("color").value;
+    width_of_line = document.getElementById("width_of_line").value;
+    radius = document.getElementById("radius").value;
+    mouseEvent = "mouseDown";
+}
+
+function my_mousemove(e) {
+    current_position_of_mouse_x = e.ClientX - canvas.offsetLeft;
+    current_position_of_mouse_y = e.ClientY - canvas.offsetTop;
+
+    if (mouseEvent == "mouseDown") {
+        console.log("Current position of X and Y coordinates = ");
+        console.log("x = " + current_position_of_mouse_x + "y = " + current_position_of_mouse_y);
+        ctx.beginPath();
+        ctx.strokeStyle - color;
+        ctx.lineWidth = width_of_line;
+        ctx.arc(current_position_of_mouse_x, current_position_of_mouse_y, radius, 0, 2 * Math.PI);
+        ctx.stroke();
+    }
+}
